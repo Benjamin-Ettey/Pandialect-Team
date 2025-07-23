18710971bce0ad836334d08070cac1e2f2506def
teddbug-00 <dicksonadanya269@gmail.com>
2025-07-23 16:20:44 +0000

    fix: update BASE_API_URL to use production endpoint and comment out local URL

--- utils/consts.ts
+++ utils/consts.ts
@@ -1,3 +1,3 @@
-// export const BASE_API_URL = "https://pandylingo-backend.onrender.com" as const
+export const BASE_API_URL = "https://pandylingo-backend.onrender.com" as const
 
-export const BASE_API_URL = "http://localhost:8080" as const
+// export const BASE_API_URL = "http://localhost:8080" as const
